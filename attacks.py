36d4438c0207451893662162f27f9c83a2653e98
Davies <ald339@login02.o2.rc.hms.harvard.edu>
2022-04-17 14:33:18 -0400

    clean up

--- attacks.py
+++ attacks.py
@@ -1,146 +1,91 @@
 from abc import ABC
 import datetime
-import torchmetrics
-import torch
+import numpy as np
+import os
+import wandb
+
+from autoattack import AutoAttack
 from cleverhans.torch.attacks.fast_gradient_method import fast_gradient_method
 from cleverhans.torch.attacks.projected_gradient_descent import (
     projected_gradient_descent,
 )
-import wandb
-import numpy as np
+import torchmetrics
+import torch
 import torch.nn.functional as F
-from autoattack import AutoAttack
-import os
 
 class Attacks(ABC):
     """
-    Handles adversarial attacks via on_val_epoch_end.
+    Defines adversarial attacks via PyTorch Lightning's `on_val_epoch_end` hook. Inherited by models.
     """
 
     def __init__(
         self,
-        # attacks
         no_adversarial_fgm=True,
         no_adversarial_pgd=True,
         no_autoattack=True,
         adv_eps=0.03,
         pgd_steps=40,
-        **kwargs
     ):
-        # adversarial
+        # set attributes
         self.adversarial_fgm = not no_adversarial_fgm
         self.adversarial_pgd = not no_adversarial_pgd
         self.autoattack = not no_autoattack
-        self.fgm_acc = torchmetrics.Accuracy()
-        self.pgd_acc = torchmetrics.Accuracy()
         self.adv_eps = adv_eps
         self.pgd_steps = pgd_steps
 
-        # make logging directory if needed
-        self.log_path = f'./autoattack_logs/{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
-        if not os.path.isdir(self.log_path):
-            os.makedirs(self.log_path)
-            print(f"creating {self.log_path} directory...")
+        # mkdir logging directory for AutoAttack if needed
+        if self.autoattack:
+            self.log_path = f'./autoattack_logs/{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
+            if not os.path.isdir(self.log_path):
+                print(f"creating {self.log_path} directory...")
+                os.makedirs(self.log_path)
+        
+        # accuracy metrics
+        self.fgm_acc = torchmetrics.Accuracy()
+        self.pgd_acc = torchmetrics.Accuracy()
 
-    # attacks at end of val epoch
     def on_validation_epoch_end(self):
+        """
+        `pytorch_lightning` hook override to insert attacks at end of val epoch.
+        """
         if self.current_epoch % 10 == 0:
-           # import ipdb; ipdb.set_trace()
-           # print("\nAttacking!\n\n")
-
-            # get entire validation set
-            '''
-            l = [x for (x, y) in self.val_dataloader()]
-            x = torch.cat(l, 0)
-            l = [y for (x, y) in self.val_dataloader()]
-            y = torch.cat(l, 0)
-            x = x.to(self.device); y = y.to(self.device)
-
-            import ipdb; ipdb.set_trace()
-            
-
-            xs = []; ys = []
-            for i in range(3):
-                batch = next(iter(self.val_dataloader()))
-                x, y = batch
-                xs.append(x.to(self.device)); ys.append(y.to(self.device))
-            x = torch.cat(xs, 0); y = torch.cat(ys, 0)
-            del xs
-            del ys
-            '''
-
-            batch = next(iter(self.val_dataloader()))
+           
+            # get validation data (TODO: currently uses just one batch.)
+            batch = next(iter(self.val_dataloader())) 
             x, y = batch
             x = x.to(self.device); y = y.to(self.device)
 
-            # for gradient tracking of image to backprop through it
-            torch.set_grad_enabled(True) # TODO need this?
+            # for pgd attack, need to backpropogate to input.
+            torch.set_grad_enabled(True)
             x.requires_grad = True
 
             if self.autoattack:
                 adversary = AutoAttack(self, norm='Linf',eps=8/255, version='rand', log_path=f'{self.log_path}/{self.logger._name}.txt')
-                x_adv = adversary.run_standard_evaluation(x, y)  
+                adversary.run_standard_evaluation(x, y)  
                 wandb.save(f'{self.log_path}/{self.logger._name}.txt')
 
             if self.adversarial_fgm:
-                loss_adv_fgm, y_hat_adv_fgm, x_adv = self.fgm_attack(x, y)
+                loss_adv_fgm, y_hat_adv_pgd, _ = self.fgm_attack(x, y)
                 self.log(
                     f"Adversarial FGM Loss \n(eps={self.adv_eps}, norm=inf)",
                     loss_adv_fgm,
-                    on_step=False, 
-                    on_epoch=True,
                 )
                 self.log(
                     f"Adversarial FGM Accuracy \n(eps={self.adv_eps}, norm=inf)",
-                    self.fgm_acc(y_hat_adv_fgm, y),
-                    on_step=False,
-                    on_epoch=True,
+                    self.fgm_acc(y_hat_adv_pgd, y),
                 )
-                if self.current_epoch % 50 == 1:
-                    # show two examples
-                    self.logger.log_metrics(
-                        {
-                            "FGM Adversarial Examples": wandb.Image(
-                                np.concatenate(
-                                    [
-                                        x_adv[0].cpu().detach().numpy(),
-                                        x_adv[1].cpu().detach().numpy(),
-                                    ],
-                                    axis=1,
-                                )
-                            )
-                        }
-                    )
+
             if self.adversarial_pgd:
-                loss_adv_pgd, y_hat_adv_pgd, x_adv = self.pgd_attack(x, y)
+                loss_adv_pgd, y_hat_adv_pgd, _ = self.pgd_attack(x, y)
                 self.log(
                     f"Adversarial PGD Loss \n(eps={self.adv_eps}, norm=inf, eps_iter={self.pgd_steps}, step_size=0.007)",
                     loss_adv_pgd,
-                    on_step=False,
-                    on_epoch=True,
                 )
                 self.log(
                     f"Adversarial PGD Accuracy \n(eps={self.adv_eps}, norm=inf, eps_iter={self.pgd_steps}, step_size=0.007)",
                     self.pgd_acc(y_hat_adv_pgd, y),
-                    on_step=False,
-                    on_epoch=True,
                 )
-                if self.current_epoch % 50 == 1:
-                    self.logger.log_metrics(
-                        {
-                            "PGD Adversarial Example": wandb.Image(
-                                np.concatenate(
-                                    [
-                                        x_adv[0].cpu().detach().numpy(),
-                                        x_adv[1].cpu().detach().numpy(),
-                                    ],
-                                    axis=1,
-                                )
-                            )
-                        }
-                    )
 
-    # attacks
     def pgd_attack(self, x, y):
         """ 
         Performs a projected gradient descent attack on the model as described in
