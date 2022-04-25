f4bebcf4a908280982fed3605a7fc20b48cf70be
Singh <tes747@login06.o2.rc.hms.harvard.edu>
2022-04-24 20:27:13 -0400

    fixes

--- custom_layers/normout.py
+++ custom_layers/normout.py
@@ -10,7 +10,7 @@ class NormOut(nn.Module, CustomLayer):
     instead of the activations themselves.
     """
     def __init__(self, use_abs: bool,
-                channel_max: bool, 
+                standard_max: bool, 
                 on_at_inference: bool, 
                 **kwargs):
 
@@ -18,7 +18,7 @@ class NormOut(nn.Module, CustomLayer):
         CustomLayer.__init__(self, custom_layer_name="NormOut")
 
         self.use_abs = use_abs
-        self.channel_max = channel_max
+        self.standard_max = standard_max
         self.on_at_inference = on_at_inference
 
         if self.use_abs:
@@ -26,10 +26,10 @@ class NormOut(nn.Module, CustomLayer):
         else:
             print("Not using absolute value of activations in NormOut!")
 
-        if self.channel_max:
-            print("Using channel max in NormOut!")
+        if self.standard_max:
+            print("Using standard max in NormOut!")
         else:
-            print("Not using channel max in NormOut!")
+            print("Using buggy max in NormOut!")
 
         
     def forward(self, x):
@@ -39,10 +39,10 @@ class NormOut(nn.Module, CustomLayer):
             else:
                 x_prime = x
 
-            if self.channel_max:
-                norm_x = x_prime / torch.max(x_prime, dim=1, keepdim=True)
-            else:
+            if self.standard_max:
                 norm_x = x_prime / torch.max(x_prime, dim=1, keepdim=True)[0]
+            else:
+                norm_x = x_prime / torch.max(x_prime, dim=1, keepdim=True)
 
             x_mask = torch.rand_like(x) < norm_x
             x = x * x_mask
